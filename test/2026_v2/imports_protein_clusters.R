5d48cf898f66eb4ad917b64c371d842b014f47d7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-13 21:07:19 +0800

    Update imports_protein_clusters.R

--- test/2026_v2/imports_protein_clusters.R
+++ test/2026_v2/imports_protein_clusters.R
@@ -1,6 +1,6 @@
 require(biocad_registry);
 
-let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.48");
 let rep = "K:\diamonds";
 let mysql = "C:\\Program Files\\MySQL\\MySQL Workbench 8.0 CE\\mysql.exe";
 let cmd = biocad_registry |> imports_sql(rep, mysql,batch_script = TRUE);
