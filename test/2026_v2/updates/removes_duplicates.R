7812e7853259a03ca5a48a3f949b261a4d141b67
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-19 15:45:24 +0800

    Update removes_duplicates.R

--- test/2026_v2/updates/removes_duplicates.R
+++ test/2026_v2/updates/removes_duplicates.R
@@ -2,4 +2,4 @@ require(biocad_registry);
 
 imports "models" from "biocad_registry";
 
-resolve_metabolite_alias(open_registry("xieguigang", 123456, host ="192.168.3.15"));
+resolve_metabolite_alias(open_registry("root", 123456, host ="192.168.3.48"));
