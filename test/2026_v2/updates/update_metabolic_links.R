5078e282a38dcd942763e869e0f98b877c059ba4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-19 16:07:09 +0800

    Update update_metabolic_links.R

--- test/2026_v2/updates/update_metabolic_links.R
+++ test/2026_v2/updates/update_metabolic_links.R
@@ -6,4 +6,4 @@ let registry = open_registry("root", 123456, host ="192.168.3.48");
 
 # Fill metabolite species id inside metabolic network
 registry |> update_metabolic_network();
-registry |> register_metabolic_symbols();
+# registry |> register_metabolic_symbols();
