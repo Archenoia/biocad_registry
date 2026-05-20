99543286de64c530a7c3ecd77895db7100126144
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-20 11:56:51 +0800

    config db script

--- scripts/make_vcdb.R
+++ scripts/make_vcdb.R
@@ -2,7 +2,7 @@ require(biocad_registry);
 
 imports "exports" from "biocad_registry";
 
-let biocad_registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
+let biocad_registry = open_registry("root", 123456, host ="192.168.3.48");
 let repo = relative_work("./localdb");
 
 biocad_registry |> exports::virtualcell_componentdb(repo,
--- test/2026_v2/updates/update_metabolic_links.R
+++ test/2026_v2/updates/update_metabolic_links.R
@@ -5,5 +5,5 @@ imports "models" from "biocad_registry";
 let registry = open_registry("root", 123456, host ="192.168.3.48");
 
 # Fill metabolite species id inside metabolic network
-# registry |> update_metabolic_network();
+registry |> update_metabolic_network();
 registry |> register_metabolic_symbols();
