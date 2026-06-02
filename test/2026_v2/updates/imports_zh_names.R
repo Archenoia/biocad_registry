736b56d7bea222638ce714728b172220ebed06ae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-02 18:29:27 +0800

    Update imports_zh_names.R

--- test/2026_v2/updates/imports_zh_names.R
+++ test/2026_v2/updates/imports_zh_names.R
@@ -3,7 +3,7 @@ require(biocad_registry);
 imports "registry" from "biocad_registry";
 
 let biocad_registry = open_registry("root", 123456, host ="192.168.3.48");
-let names = read.csv("Z:\lactate_new.csv", row.names = NULL, check.names = FALSE);
+let names = read.csv("Z:\zh_names\Procyanidin_new.csv", row.names = NULL, check.names = FALSE);
 
 print(names);
 
