f14ef5ee133ee36ed7e7af3b87a289e405578509
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-23 22:24:38 +0800

    Update ncbi_taxonomy.R

--- test/2026_v2/ncbi_taxonomy.R
+++ test/2026_v2/ncbi_taxonomy.R
@@ -3,7 +3,13 @@ require(biocad_registry);
 imports "setup" from "biocad_registry";
 imports "taxonomy_kit" from "metagenomics_kit";
 
-let registry = open_registry("xieguigang", 123456, host ="192.168.3.15");
-let ncbi_tax = Ncbi.taxonomy_tree("U:\metagenomics_LLMs\taxdmp_2025-12-01");
+let registry = open_registry("root", 123456, host ="192.168.3.48");
+# let ncbi_tax = Ncbi.taxonomy_tree("U:\metagenomics_LLMs\taxdmp_2025-12-01");
+let tax_map = read.table("C:\Users\Administrator\Downloads\NCBI2GTDB_lineage.tsv", header = TRUE);
 
-setup::setup_taxonomy(registry,ncbi_tax);
+# setup::setup_taxonomy(registry,ncbi_tax);
+
+print(tax_map, max.print = 6);
+stop();
+
+map_ncbi_gtdb_taxonomy(registry, ncbi_tax, GTDB_tax);
