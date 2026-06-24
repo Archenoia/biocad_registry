9fb6a0ef4fa8cc9503c7e3697873cd30272c236d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-24 09:16:28 +0800

    make batch imports all

--- test/2026_v2/imports_metaTraits.R
+++ test/2026_v2/imports_metaTraits.R
@@ -5,6 +5,15 @@ imports "models" from "biocad_registry";
 imports "microbiome" from "metagenomics_kit";
 
 let registry = open_registry("root", 123456, host ="192.168.3.48");
-let traisdata = load.meta_traits("C:\Users\Administrator\Downloads\ncbi_species_summary_no_predictions.tsv");
 
-registry |> imports_metatraits(traisdata );
+for(file in ["C:\Users\Administrator\Downloads\ncbi_species_summary_no_predictions.tsv"
+"C:\Users\Administrator\Downloads\ncbi_genus_summary_all.tsv"
+"C:\Users\Administrator\Downloads\ncbi_family_summary_all.tsv"
+"C:\Users\Administrator\Downloads\ncbi_species_summary_all.tsv"
+]) {
+
+    let traisdata = load.meta_traits(file);
+
+    registry |> imports_metatraits(traisdata );
+}
+
